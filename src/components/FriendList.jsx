d46643522da9d43d4e42920af62d588dfa85db00
Danaproject <decoratio@gmail.com>
2021-04-14 21:50:34 +0200

    + FriendList defaultAvatar

--- src/components/FriendList.jsx
+++ src/components/FriendList.jsx
@@ -1,5 +1,6 @@
 import FriendListItem from './FriendListItem';
 import PropTypes from "prop-types";
+import defaultAvatar from '../default.svg';
 
 const FriendList = ({ friends }) => {
     return (
@@ -11,11 +12,16 @@ const FriendList = ({ friends }) => {
     );
 };
 
+FriendListItem.defaultProps = {
+    avatar: defaultAvatar,
+    // avatar: "https://www.flaticon.com/svg/static/icons/svg/3135/3135715.svg",
+};
+
 FriendList.propTypes = {
     friends: PropTypes.arrayOf(
         PropTypes.shape({
         id: PropTypes.number.isRequired,
-        avatar: PropTypes.string.isRequired,
+        avatar: PropTypes.string,
         name: PropTypes.string.isRequired,
         isOnline: PropTypes.bool.isRequired,
         })
