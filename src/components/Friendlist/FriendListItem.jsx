cb0e5960bd8f95ef0247300546f6cbf4d8d480f8
Danaproject <decoratio@gmail.com>
2021-04-16 08:34:19 +0200

    Friendlist status, variables

--- src/components/Friendlist/FriendListItem.jsx
+++ src/components/Friendlist/FriendListItem.jsx
@@ -2,7 +2,7 @@ import styles from './FriendList.module.css';
 
 const FriendListItem = ({ avatar, name, isOnline }) => (
     <li className={styles.item}>
-        <span className="styles.status">{isOnline}</span>
+        <span className={isOnline ? styles.isOnline : styles.status}></span>
         <img className={styles.avatar} src={avatar} alt="Friend avatar" width="48" />
         <p className={styles.name}>{name}</p>
     </li>
