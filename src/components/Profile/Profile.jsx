fc3967751a981db968e498a719856111dae647a7
Danaproject <decoratio@gmail.com>
2021-04-16 09:02:27 +0200

    variables

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -17,15 +17,15 @@ const Profile = ({ avatar, name, tag, location, stats }) => (
         </div>
 
         <ul className={styles.stats}>
-            <li className={styles.stats__item}>
+            <li className={styles.statsItem}>
             <span className={styles.label}>Followers</span>
             <span className={styles.quantity}>{stats.followers}</span>
             </li>
-            <li className={styles.stats__item}>
+            <li className={styles.statsItem}>
             <span className={styles.label}>Views</span>
             <span className={styles.quantity}>{stats.views}</span>
             </li>
-            <li className={styles.stats__item}>
+            <li className={styles.statsItem}>
             <span className={styles.label}>Likes</span>
             <span className={styles.quantity}>{stats.likes}</span>
             </li>
